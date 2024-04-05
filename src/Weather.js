d918522af7f992fb7e1cfc2c5cf199267d3064a7
MateaZerjav <150837054+MateaZerjav@users.noreply.github.com>
2024-04-05 14:10:26 +0200

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,55 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <form>
-          <div className="row mt-4">
-            <div className="col-9 ">
-              <input
-                type="sertch"
-                placeholder="Enter a city...."
-                className="form-control"
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Submit"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
+  const [weather, setWeather] = useState({ ready: false });
 
-        <h2 className="mt-4">New York</h2>
-        <ul className="mt-3 ">
-          <li>Wednesday 07:00</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <div className="col-6 mt-4 mb-4 text-center">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png"
-                alt=""
-              />
-              <span className="temperature ">18</span>°C
+  function handleResponse(response) {
+    setWeather({
+      ready: true,
+      date: "Wednesday 07:00",
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      name: response.data.name,
+      description: response.data.weather[0].description,
+      precipitation: 50,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png",
+    });
+    console.log(response.data);
+  }
+  if (weather.ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <form>
+            <div className="row mt-4">
+              <div className="col-9 ">
+                <input
+                  type="search"
+                  placeholder="Enter a city...."
+                  className="form-control"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-primary w-100"
+                />
+              </div>
             </div>
-          </div>
+          </form>
+          <h2 className="mt-4">{weather.name}</h2>
+          <ul className="mt-3 ">
+            <li>{weather.date}</li>
+            <li className="text-capitalize">{weather.description}</li>
+          </ul>
 
-          <div className="col-6 ">
-            <ul className="properties">
-              <li>Precipitation: 50%</li>
-              <li>Humidity:50%</li>
-              <li>Wind: 13 km/h</li>
-            </ul>
+          <div className="row">
+            <div className="col-6">
+              <div className="col-6 mt-4 mb-4 text-center">
+                <img src={weather.icon} alt="" />
+                <span className="temperature ">
+                  {Math.round(weather.temperature)}
+                </span>
+                °C
+              </div>
+            </div>
+
+            <div className="col-6 ">
+              <ul className="properties">
+                <li>Precipitation: {weather.precipitation}%</li>
+                <li>Humidity: {weather.humidity}%</li>
+                <li>Wind: {weather.wind} km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let city = "New York";
+    const apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
